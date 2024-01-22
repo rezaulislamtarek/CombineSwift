533c110f5ec6ee537056ce89b1d3e64da9ff128e
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-01-22 18:40:44 +0600

    array to publisher

--- CombineSwift.playground/Contents.swift
+++ CombineSwift.playground/Contents.swift
@@ -1,8 +1,18 @@
 import UIKit
 import Combine
 
-var publisher = Just("Hello World")
-let cancellable = publisher.sink { value in
+/*
+ var publisher = Just("Hello World")
+ let cancellable = publisher.sink { value in
+ print(value)
+ }
+ 
+ cancellable.cancel()
+ */
+
+let numberPublisher = [1,2,3,4,5,6].publisher
+let doublePublisher = numberPublisher.map { $0 * 2 }
+let cancellable = doublePublisher.sink { value in
     print(value)
 }
 
