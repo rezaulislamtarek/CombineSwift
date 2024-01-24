180cf35b9a0ea3aa7ee3b9079d77ec6b1be6413b
rezaulislam <mrezaulislam.t@gmail.com>
2024-01-24 18:34:00 +0600

    cancellable

--- CombineSwift.playground/Contents.swift
+++ CombineSwift.playground/Contents.swift
@@ -1,4 +1,14 @@
 import UIKit
 import Combine
 
-let numberPublisher = [1...10].publisher
+ 
+let publisher = Timer.publish(every: 1.0, on: .main, in: .default)
+     
+let cancellable = publisher.autoconnect().sink { v in
+    print("v -> \(v)")
+}
+
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 5.0){
+    cancellable.cancel()
+}
