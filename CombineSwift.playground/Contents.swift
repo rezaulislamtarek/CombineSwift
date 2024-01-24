a44042922ef72a81de23ca2108a8438166de9220
rezaulislam <mrezaulislam.t@gmail.com>
2024-01-24 17:33:09 +0600

    init

--- CombineSwift.playground/Contents.swift
+++ CombineSwift.playground/Contents.swift
@@ -1,19 +1,4 @@
 import UIKit
 import Combine
 
-/*
- var publisher = Just("Hello World")
- let cancellable = publisher.sink { value in
- print(value)
- }
- 
- cancellable.cancel()
- */
-
-let numberPublisher = [1,2,3,4,5,6].publisher
-let doublePublisher = numberPublisher.map { $0 * 2 }
-let cancellable = doublePublisher.sink { value in
-    print(value)
-}
-
-cancellable.cancel()
+let numberPublisher = [1...10].publisher
